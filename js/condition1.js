61c682b929afe1975ba7033f5b27dc26b98765fe
JB Lee <dkdlskqn1@naver.com>
2025-04-25 15:55:00 +0900

    jb

---
+++ js/condition1.js
@@ -0,0 +1,19 @@
+// condition.js
+let isTrue = true;
+let num1 = 5;
+let num2 = 10;
+
+isTrue = 3 < 5;
+isTrue = num1 < 5;
+isTrue = --num1 < 5;
+isTrue = --num1 <= 4 && num2++ == 10;
+isTrue = 2;  // true / false => truthy / falsy (0, "", null, underfied, NaN)
+console.log(num1, num2);
+
+if (isTrue) {
+  // isTrue 의 참 => if 블럭 실행.
+  console.log('참');
+} else {
+  // isTrue 의 참 => else 블럭 실행.
+  console.log('거짓')
+}
--- js/variable3.js
+++ js/variable3.js
@@ -5,6 +5,9 @@ let num2 = document.querySelector('#num2').value;
 num1 = parseInt(num1); // "30" -> 30
 num2 = parseInt(num2); // "17" -> 17
 num1++; // 31
+num1++; // 32
+num1++; // 33
+
 
 //let result = num1 + num2; // "30" + "17" = "3017";   parseInt = 문자를 숫자로 바꾸는것.
 let result = num1++ % --num2; // "30" + "17" = "47";
